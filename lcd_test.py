858e3ef2f7448cdc213d81d5d05065a21c4ae880
giditre <giditre@gmail.com>
2020-05-17 15:53:09 +0100

    initial commit

---
+++ lcd_test.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____
+#   / _ \/ _ \(_) __/__  __ __
+#  / , _/ ___/ /\ \/ _ \/ // /
+# /_/|_/_/  /_/___/ .__/\_, /
+#                /_/   /___/
+#
+#  lcd_16x2.py
+#  16x2 LCD Test Script
+#
+# Author : Matt Hawkins
+# Date   : 06/04/2015
+#
+# http://www.raspberrypi-spy.co.uk/
+#
+# Copyright 2015 Matt Hawkins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#--------------------------------------
+
+# The wiring for the LCD is as follows:
+# 1 : GND
+# 2 : 5V
+# 3 : Contrast (0-5V)*
+# 4 : RS (Register Select)
+# 5 : R/W (Read Write)       - GROUND THIS PIN
+# 6 : Enable or Strobe
+# 7 : Data Bit 0             - NOT USED
+# 8 : Data Bit 1             - NOT USED
+# 9 : Data Bit 2             - NOT USED
+# 10: Data Bit 3             - NOT USED
+# 11: Data Bit 4
+# 12: Data Bit 5
+# 13: Data Bit 6
+# 14: Data Bit 7
+# 15: LCD Backlight +5V**
+# 16: LCD Backlight GND
+
+#import
+import RPi.GPIO as GPIO
+import time
+import argparse
+
+# Define GPIO to LCD mapping
+LCD_RS = 3
+LCD_E  = 5
+LCD_D4 = 7
+LCD_D5 = 8
+LCD_D6 = 10
+LCD_D7 = 12
+
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+def main():
+  # Main program block
+  
+  #GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BOARD)     # use board pin numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+  # Initialise display
+  lcd_init()
+
+  while True:
+
+    text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+
+    # Send some test
+    lcd_text(text, 2)
+
+    # some delay
+    # time.sleep(3)
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+
+  GPIO.output(LCD_RS, mode) # RS
+
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+
+def lcd_string(message, line):
+
+  message = message.ljust(LCD_WIDTH, " ")
+
+  if len(message) > LCD_WIDTH:
+    print("WARNING: message {} chars, line is {} chars".format(len(message), LCD_WIDTH))
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]), LCD_CHR)
+
+def lcd_text(message, page_interval=3):
+  # divide message in chunks of length LCD_WIDTH
+
+  # set first line to write on
+  next_line = LCD_LINE_1
+  # set cursor position in message
+  start_position = 0
+  while len(message)-start_position > LCD_WIDTH:
+    # compute end position
+    end_position = start_position + LCD_WIDTH
+    lcd_string(message[start_position:end_position], next_line)
+    # set next start position in message
+    start_position = end_position
+    # set next line on LCD
+    next_line = LCD_LINE_2 if next_line == LCD_LINE_1 else LCD_LINE_1
+    if next_line == LCD_LINE_1:
+      time.sleep(page_interval)
+
+  lcd_string(message[start_position:], next_line)
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
+    lcd_string("Goodbye!",LCD_LINE_1)
+    GPIO.cleanup()
---
+++ led_matrix_test.py
@@ -0,0 +1,242 @@
+import RPi.GPIO as GPIO
+import time
+import argparse
+import json
+
+# Define GPIO to REG mapping
+REG_D = 11
+REG_ST = 13
+REG_SH = 15
+
+#PULSE_DURATION = 0.000000001
+#
+#row_to_index = [15, 10, 9, 12, 1, 7, 2, 5]
+#column_to_index = [11, 3, 4, 14, 6, 13, 8, 0]
+#reorder_array = row_to_index + column_to_index
+#reorder_array_inverted = [ 15-i for i in reorder_array ]
+#
+#index_to_position = [
+#  "R1",
+#  "C4",
+#  "C6",
+#  "R4",
+#  "C1",
+#  "R2",
+#  "R3",
+#  "C7",
+#  "R6",
+#  "C5",
+#  "R8",
+#  "C3",
+#  "C2",
+#  "R7",
+#  "R5",
+#  "C8"
+#]
+
+position_dict = {
+  "R1": 0,
+  "C4": 1,
+  "C6": 2,
+  "R4": 3,
+  "C1": 4,
+  "R2": 5,
+  "R3": 6,
+  "C7": 7,
+  "R6": 8,
+  "C5": 9,
+  "R8": 10,
+  "C3": 11,
+  "C2": 12,
+  "R7": 13,
+  "R5": 14,
+  "C8": 15
+}
+
+with open("char_sets.json") as f:
+  char_sets = json.load(f)
+
+def store():
+  GPIO.output(REG_ST, GPIO.HIGH)
+  #time.sleep(PULSE_DURATION)
+  GPIO.output(REG_ST, GPIO.LOW)
+  #time.sleep(PULSE_DURATION)
+
+def shift():
+  GPIO.output(REG_SH, GPIO.HIGH)
+  #time.sleep(PULSE_DURATION)
+  GPIO.output(REG_SH, GPIO.LOW)
+  #time.sleep(PULSE_DURATION)
+
+def out_word(word):
+  for i in range(len(word)):
+    GPIO.output(REG_D, word[i])
+    shift()
+  store()
+
+def clear_matrix():
+  word = 16 * [False]
+  out_word(word)
+
+def light_dot(r, c):
+  word = 16 * [False]
+  word[position_dict["R{}".format(r+1)]] = True
+  for i in range(8):
+    word[position_dict["C{}".format(i+1)]] = True
+  word[position_dict["C{}".format(c+1)]] = False
+  #print(word)
+  out_word(word)
+  #input("Press Enter...")
+
+def light_row(row_n, row_values):
+  word = 16 * [False]
+  word[position_dict["R{}".format(row_n+1)]] = True
+  for c in range(8):
+    word[position_dict["C{}".format(c+1)]] = not row_values[c]
+  out_word(word)  
+
+def display_image_by_dot(image, persistence=0):
+  if persistence > 0:
+    end_t = time.time() + persistence
+  while True:
+    for c in range(8):
+      for r in range(8):
+        if image[r][c]:
+          light_dot(r,c)
+    if persistence > 0 and time.time() >= end_t:
+      break
+
+def display_image_by_row(image, persistence=0):
+  if persistence > 0:
+    end_t = time.time() + persistence
+  while True:
+    for r in range(8):
+      #print("row", r, image[r])
+      light_row(r, image[r])
+      #input("Press Enter...")
+    if persistence > 0 and time.time() >= end_t:
+      break
+
+def display_large_image(image, persistence=1):
+  # this function is used to display images wider than 8 pixels
+  # the height is still assumed to be 8 pixels
+  # take image width as lenght of the first row
+  image_width = len(image[0])
+  #print("image_width", image_width)
+  start_c = 0
+  while start_c <= image_width-8:
+    end_c = start_c + 8
+    #print(start_c, end_c)
+    frame = [ [ image[r][c] for c in range(start_c, end_c) ] for r in range(8) ]
+    #print("frame", frame)
+    display_image_by_row(frame, persistence)
+    start_c += 1
+      
+def hex_to_image(hex_string):
+  #print("hex_string", hex_string)
+  binary_string = "{0:064b}".format(int(hex_string, 16))
+  #print("binary_string", binary_string)
+  rev_binary_string = binary_string[::-1]
+  #print("rev binary_string", rev_binary_string)
+  image = [ [ int(b) for b in rev_binary_string[i:i+8] ] for i in range(0, 64, 8) ]
+  return image
+
+def create_large_image(hex_images):
+  large_image = [ [] for r in range(8) ]
+  for hex_string in hex_images:
+    image = hex_to_image(hex_string)
+    for r in range(8):
+      large_image[r] += image[r]
+  return large_image
+
+def display_char(char, char_set="set1", persistence=0):
+  hex_string = char_sets["{}_{}".format(char_set, char)]
+  image = hex_to_image(hex_string)
+  display_image_by_row(image, persistence)
+
+def display_string_static(text, char_set="set1", persistence=1):
+  for char in text:
+    display_char(char, char_set=char_set, persistence=persistence)
+
+def display_string_scroll(text, char_set="set1", persistence=1):
+  hex_chars = []
+  for char in text:
+    hex_chars.append(char_sets["{}_{}".format(char_set, char)])
+  image = create_large_image(hex_chars)
+  # add left and right blanks
+  for r in range(8):
+    image[r] = 8*[0] + image[r] + 8*[0]
+  display_large_image(image, persistence=persistence)
+
+def main():
+  
+  #GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BOARD)     # use board pin numbers
+  GPIO.setup(REG_D, GPIO.OUT)  # D
+  GPIO.setup(REG_ST, GPIO.OUT) # ST
+  GPIO.setup(REG_SH, GPIO.OUT) # SH
+
+  GPIO.output(REG_SH, GPIO.LOW)
+  GPIO.output(REG_ST, GPIO.LOW)  
+
+  clear_matrix()
+
+  #word[position_dict["R1"]] = True
+  #word[position_dict["R8"]] = True
+  #for i in range(1, 8+1):
+  #  word[position_dict["C{}".format(i)]] = True
+  #word[position_dict["C1"]] = False
+  #word[position_dict["C8"]] = False
+  #print(word)
+  #out_word(word)
+  #input("Press Enter...")
+
+
+  #light_dot(5, 8)
+  #for i in range(8):
+  #  light_row(i, image[i])
+  #  input("Press Enter...")
+  #display_image_by_row(image)
+
+  image_G = hex_to_image("0xffffc3fbfb03ffff")
+  image_skull = hex_to_image("0x2a3e367f49497f3e")
+  image_heart = hex_to_image("0x183c7effffffff66")
+  
+  #display_image_by_row(image_heart)
+  
+  #display_char("K")
+
+  #display_string_static("Ciao")
+
+  #large_image = create_large_image(["0x183c7effffffff66", "0x2a3e367f49497f3e"])
+  #print(large_image)
+  #display_large_image(large_image, persistence=0.5)
+  
+
+  #clear_matrix()
+
+  while True:
+
+    display_string_scroll("send nudes", persistence=0.1)
+    #display_image_by_row(image_skull, persistence=1)
+
+    time.sleep(3)
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser()
+  
+  #parser.add_argument("mand-arg", help="Mandatory positional argument.")
+  parser.add_argument("-c", "--char", help="Character to be displayed", required=False)
+  #parser.add_argument("-b", "--opt-arg-b", help="Optional string argument, default: 3", type=int, nargs="?", default=3)
+  #parser.add_argument("-c", "--opt-arg-c", help="Optional boolean argument, default: False", action="store_true", default=False)
+  
+  args = parser.parse_args()
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    clear_matrix()
+    GPIO.cleanup()
