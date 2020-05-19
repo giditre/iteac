7151ee8bc4f9adde83fcfbb2771dc1ebb0a08ab7
giditre <giditre@gmail.com>
2020-05-19 19:54:41 +0100

    commit from iteac on 19/05/2020 at 19:54

---
+++ pir_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+PIR_PIN = 37
+
+GPIO.setmode(GPIO.BOARD)
+
+#Read output from PIR motion sensor
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+while True:
+  if GPIO.input(PIR_PIN):
+    print("Chi siete?")
+    t = 0
+    while GPIO.input(PIR_PIN):
+      print(".", end='', flush=True)
+      t += 1
+      time.sleep(0.1)
+    print("Un fiorino!")
+    print(t)
+  time.sleep(0.1)
+
